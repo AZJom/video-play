488b8c6e10e75bca0d4f543542c8bbd6483a139c
AZJom <144042592+AZJom@users.noreply.github.com>
2023-11-06 04:40:26 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,64 +1,69 @@
-var video;
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("player1");
+    var playButton = document.getElementById("play");
+    var pauseButton = document.getElementById("pause");
+    var slowerButton = document.getElementById("slower");
+    var fasterButton = document.getElementById("faster");
+    var skipButton = document.getElementById("skip");
+    var muteButton = document.getElementById("mute");
+    var volumeSlider = document.getElementById("slider");
+    var volumeText = document.getElementById("volume");
+    var vintageButton = document.getElementById("vintage");
+    var originalButton = document.getElementById("orig");
 
-window.addEventListener("load", function() {
-    console.log("Good job opening the window");
-    video = document.getElementById("player1");
-    video.autoplay = false;
-    video.loop = false;
-});
+    playButton.addEventListener("click", function() {
+        if (video.paused) {
+            video.play();
+            updateVolume();
+        }
+    });
 
-document.querySelector("#play").addEventListener("click", function() {
-    if (video.paused) {
-        video.play();
-        updateVolume();
-    }
-});
+    pauseButton.addEventListener("click", function() {
+        if (!video.paused) {
+            video.pause();
+        }
+    });
 
-document.querySelector("#pause").addEventListener("click", function() {
-    if (!video.paused) {
-        video.pause();
-    }
-});
-
-document.querySelector("#slower").addEventListener("click", function() {
-    video.playbackRate -= 0.1;
-    console.log("New Speed: ", video.playbackRate);
-});
-
-document.querySelector("#faster").addEventListener("click", function() {
-    if (video.playbackRate < 1.0) {
-        video.playbackRate += 0.1;
+    slowerButton.addEventListener("click", function() {
+        video.playbackRate -= 0.1;
         console.log("New Speed: ", video.playbackRate);
+    });
+
+    fasterButton.addEventListener("click", function() {
+        if (video.playbackRate < 1.0) {
+            video.playbackRate += 0.1;
+            console.log("New Speed: ", video.playbackRate);
+        }
+    });
+
+    skipButton.addEventListener("click", function() {
+        if (video.currentTime + 10 < video.duration) {
+            video.currentTime += 10;
+        } else {
+            video.currentTime = 0;
+        }
+        console.log("Current Time: ", video.currentTime);
+    });
+
+    muteButton.addEventListener("click", function() {
+        video.muted = !video.muted;
+        muteButton.textContent = video.muted ? "Unmute" : "Mute";
+    });
+
+    volumeSlider.addEventListener("input", function() {
+        video.volume = volumeSlider.value / 100;
+        updateVolume();
+    });
+
+    vintageButton.addEventListener("click", function() {
+        video.classList.add("oldSchool");
+    });
+
+    originalButton.addEventListener("click", function() {
+        video.classList.remove("oldSchool");
+    });
+
+    function updateVolume() {
+        volumeText.textContent = (video.volume * 100).toFixed(0) + "%";
     }
 });
-
-document.querySelector("#skip").addEventListener("click", function() {
-    if (video.currentTime + 10 < video.duration) {
-        video.currentTime += 10;
-    } else {
-        video.currentTime = 0;
-    }
-    console.log("Current Time: ", video.currentTime);
-});
-
-document.querySelector("#mute").addEventListener("click", function() {
-    video.muted = !video.muted;
-    this.textContent = video.muted ? "Unmute" : "Mute";
-});
-
-document.querySelector("#slider").addEventListener("input", function() {
-    video.volume = this.value / 100;
-    updateVolume();
-});
-
-document.querySelector("#vintage").addEventListener("click", function() {
-    video.classList.add("oldSchool");
-});
-
-document.querySelector("#orig").addEventListener("click", function() {
-    video.classList.remove("oldSchool");
-});
-
-function updateVolume() {
-    document.querySelector("#volume").textContent = (video.volume * 100).toFixed(0) + "%";
-}
